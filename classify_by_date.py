74c56b11512e66e1713b8f58dd300dd0a09dcbab
snqzspg <snqzspg@gmail.com>
2025-01-20 16:47:24 +0800

    Added warnings if an image does not have exif information.
    Ported from snapshot 202306262348.

--- classify_by_date.py
+++ classify_by_date.py
@@ -3,14 +3,15 @@ from argparse import ArgumentParser
 from datetime import datetime
 from functools import reduce
 import logging
-from os import listdir, mkdir, path
+from os import get_terminal_size, linesep, listdir, mkdir, path
 from shutil import move
 from subprocess import check_output
 from typing import List, Optional, Tuple
 
-SNAPSHOT_VERSION = "202306260146"
+SNAPSHOT_VERSION = "202306262348"
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
 EXIFTOOL_COMMAND:str = "exiftool"
+# EXIFTOOL_COMMAND = path.join(SCRIPT_DIR, "exiftool")
 
 def date_folder_name_fmt(dt:datetime) -> str:
 	# return f'{dt.year}-{dt.month}-{dt.day}'
@@ -19,26 +20,48 @@ def date_folder_name_fmt(dt:datetime) -> str:
 def get_exiftool_date_info_iphone(img:str) -> Optional[str]:
 	command = [EXIFTOOL_COMMAND, '-T', img]
 	info = check_output(command + ['-CreationDate']).decode(encoding = "utf-8")
-	info = info.split('\n')[0]
+	info = info.split(linesep)[0]
 	info = info.split('+')[0]
 	if info == '-':
 		info = check_output(command + ['-DateTimeCreated']).decode(encoding = "utf-8")
-		info = info.split('\n')[0]
+		info = info.split(linesep)[0]
 	if info == '-':
 		info = check_output(command + ['-DateTimeOriginal']).decode(encoding = "utf-8")
-		info = info.split('\n')[0]
+		info = info.split(linesep)[0]
 		info = info.split('+')[0]
 	if info == '-':
 		return None
 	return info
 
+def fit_one_line(s:str, keep_last_n_chars:int = 0, n_dots_ellipsis:int = 3) -> str:
+	w = get_terminal_size().columns
+	if w > 1:
+		w -= 1
+	sl = len(s)
+	if sl <= w:
+		return s + (' ' * (w - sl))
+	if w <= n_dots_ellipsis:
+		return s[:w + 1]
+	if w <= keep_last_n_chars + n_dots_ellipsis:
+		return s[:w - n_dots_ellipsis] + '.' * n_dots_ellipsis
+	return s[:w - keep_last_n_chars - n_dots_ellipsis] + ('.' * n_dots_ellipsis) + ("" if keep_last_n_chars == 0 else s[-keep_last_n_chars:])
+
+def clean_printing_line() -> None:
+	print(' ' * (get_terminal_size().columns - 1), end = "\r")
+
 def parse_exiftool_datetime(exiftooldate:str) -> Tuple[int, int, int, int, int, int]:
 	return tuple(map(lambda a: int(a), reduce(lambda a, b: a + b, map(lambda a: a.split(':'), exiftooldate.split(" ")))))
 
-def get_exif_date_time(img:str) -> datetime:
+def get_exif_date_time(img:str, progress:int, goal:int) -> datetime:
 	exif_date_str = get_exiftool_date_info_iphone(img)
+	if logging.root.isEnabledFor(logging.INFO):
+		print(fit_one_line(f"[{progress}/{goal}] Processing \"{img}\""))
 	if not exif_date_str or exif_date_str == '-':
-		return datetime.fromtimestamp(path.getmtime(img))
+		last_mod_date = datetime.fromtimestamp(path.getmtime(img))
+		clean_printing_line()
+		logging.info(f"Exiftool did not give a date for \"{img}\", using last modified date instead.")
+		logging.info("Last modified date: " + last_mod_date.strftime("%Y:%m:%d %H:%M:%S"))
+		return last_mod_date
 	y, m, d, h, mins, secs = parse_exiftool_datetime(exif_date_str)
 	return datetime(y, m, d, h, mins, secs)
 
@@ -76,7 +99,8 @@ def main() -> None:
 			logging.warning(f"Path '{folder}' given is not a directory.")
 			continue
 		files = [i for i in listdir(folder) if path.isfile(path.join(folder, i))]
-		dates = [get_exif_date_time(path.join(folder, i)) for i in files]
+		goal = len(files)
+		dates = [get_exif_date_time(path.join(folder, f), i, goal) for i, f in enumerate(files)]
 		classify_by_date(folder, files, dates)
 
 if __name__ == '__main__':
